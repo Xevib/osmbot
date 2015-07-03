e9b035e936e38a15dad17769e6144960980f20f8
Xavier Barnada <xbarnada@gmail.com>
2015-07-03 20:14:12 +0200

    Add file with token and make bot a class

---
+++ src/bot.py
@@ -0,0 +1,34 @@
+import json
+import requests
+
+class OSMbot(object):
+    def __init__(self, token):
+        self.token=token
+        self.url = "https://api.telegram.org/bot{0}/{1}"
+
+    def getUpdates(self, offset=None, limit=None, timeout=None):
+        method = "getUpdates"
+        params = {
+            'offset': offset,
+            'limit': limit,
+            'timeout': timeout
+        }
+        return json.loads(requests.get(self.url.format(self.token,method),params=params).content)
+
+    def setWebhook(self):
+        method = "setWebhook"
+        params = {}
+        response = requests.get(self.url.format(self.token,method),params=params)
+        return response.content
+
+    def sendMessage(self, chat_id, text, disable_web_page_preview=None, reply_to_message_id=None, reply_markup=None):
+        method = "sendMessage"
+        params = {
+            'chat_id': chat_id,
+            'text':text,
+            'disable_web_page_preview':disable_web_page_preview,
+            'reply_to_message_id':reply_to_message_id,
+            'reply_markup':reply_markup
+        }
+
+        return json.loads(requests.get(self.url.format(self.token, method), params=params).content)
--- src/main.py
+++ src/main.py
@@ -1,49 +1,15 @@
 # -*- coding: utf-8 -*-
-import requests
-import json
 import re
 import nominatim
 import os
 import sched, time
 from osmapi import OsmApi
-
-class OSMbot(object):
-    def __init__(self,token):
-        self.token=token
-        self.url = "https://api.telegram.org/bot{0}/{1}"
-
-    def getUpdates(self,offset=None,limit=None,timeout=None):
-        method = "getUpdates"
-        params={
-            'offset':offset,
-            'limit':limit,
-            'timeout':timeout
-        }
-        return json.loads(requests.get(self.url.format(self.token,method),params=params).content)
-
-    def setWebhook(self):
-        method = "setWebhook"
-        params = {}
-        response = requests.get(self.url.format(self.token,method),params=params)
-        return response.content
-
-    def sendMessage(self,chat_id,text,disable_web_page_preview=None,reply_to_message_id=None,reply_markup=None):
-        method = "sendMessage"
-        params = {
-            'chat_id': chat_id,
-            'text':text,
-            'disable_web_page_preview':disable_web_page_preview,
-            'reply_to_message_id':reply_to_message_id,
-            'reply_markup':reply_markup
-        }
-
-        return json.loads(requests.get(self.url.format(self.token, method),params=params).content)
-
+from bot import OSMbot
 
 
 def attend(sc):
     if os.path.isfile("last.id"):
-        f = open("last.id","r")
+        f = open("last.id", "r")
         last_id = int(f.read())
         f.close()
         updates = bot.getUpdates(offset=last_id+1)
@@ -85,11 +51,16 @@ def attend(sc):
             f.close()
     sc.enter(15, 1, attend, (sc,))
 
+f = open("token", "r")
+token = f.read()
+f.close()
+
 api = OsmApi()
 nom = nominatim.Nominatim()
-bot = OSMbot("token")
+bot = OSMbot(token)
+
 s = sched.scheduler(time.time, time.sleep)
-s.enter(1,1, attend, (s,))
+s.enter(1, 1, attend, (s,))
 s.run()
 
 
