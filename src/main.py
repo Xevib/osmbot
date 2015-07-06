f705f837287c55b11922136de8fd1e995ad8578b
Xavier Barnada <xbarnada@gmail.com>
2015-07-06 08:34:11 +0200

    Add details command

--- src/main.py
+++ src/main.py
@@ -25,7 +25,31 @@ def attend(sc):
                     message = message[8:]
                 if message == "/start":
                     response = "Hi,how I can help you?"
-                elif message.startswith( "/about"):
+                elif message.startswith("/details"):
+                    id = message[8:].strip()
+                    osm_data = api.NodeGet(int(id))
+                    if osm_data is None:
+                        osm_data = api.WayGet(int(id))
+                    if osm_data is None:
+                        response = 'Sorry but I couldn\'t find any result for "{0}" \xF0\x9F\x98\xA2\nBut you can try to improve OpenStreetMap\xF0\x9F\x94\x8D\nhttp://www.openstreetmap.org'.format(search)
+                    else:
+                        if 'name' in osm_data['tag']:
+                            response = str(osm_data['tag']['name'])
+                        if 'addr:housenumber' in osm_data['tag'] or 'addr:street' in osm_data['tag'] or 'addr:city' in osm_data['tag'] or 'addr:country' in osm_data['tag']:
+                            response += "\n"
+                            if 'addr:housenumber' in osm_data['tag']:
+                                response += osm_data['tag']['addr:housenumber']+" "
+                            if 'addr:street' in osm_data['tag']:
+                                response += osm_data['tag']['addr:street']+" "
+                            if 'addr:city' in osm_data['tag']:
+                                response += osm_data['tag']['addr:city']+" "
+                            if 'addr:country' in osm_data['tag']:
+                                response += osm_data['tag']['addr:country']+" "
+                        bot.sendMessage(usr_id, response,disable_web_page_preview='true')
+                        if 'phone' in osm_data['tag']:
+                            response = str(osm_data['tag']['phone'])
+
+                elif message.startswith("/about"):
                     response = "OpenStreetMap bot info:\n\nCREDITS&CODE\n\xF0\x9F\x91\xA5 Author: OSM català (Catalan OpenStreetMap community)\n\xF0\x9F\x94\xA7 Code: https://github.com/Xevib/osmbot\n\xE2\x99\xBB License: GPLv3, http://www.gnu.org/licenses/gpl-3.0.en.html\n\nNEWS\n\xF0\x9F\x90\xA4 Twitter: https://twitter.com/osmbot_telegram\n\nRATING\n\xE2\xAD\x90 Rating&reviews: http://storebot.me/bot/osmbot\n\xF0\x9F\x91\x8D Please rate me at: https://telegram.me/storebot?start=osmbot\n\nThanks for use @OSMbot!!"
                 elif re.match("/search.*",message) is not None and message[8:] != "":
                     search = message[8:].replace("\n","").replace("\r","")
@@ -37,7 +61,7 @@ def attend(sc):
                         for result in results:
                             response += "\xF0\x9F\x93\xAE "+result["display_name"]+"\n"
                             try:
-                                if result['osm_type']=='node':
+                                if result['osm_type'] == 'node':
                                     osm_data = api.NodeGet(int(result['osm_id']))
                                 else:
                                     osm_data = api.WayGet(int(result['osm_id']))
@@ -50,6 +74,7 @@ def attend(sc):
                                 response = ""
                             response += "\xF0\x9F\x93\x8D http://www.openstreetmap.org/?minlat={0}&maxlat={1}&minlon={2}&maxlon={3}&mlat={4}&mlon={5}\n".format(result['boundingbox'][0],result['boundingbox'][1],result['boundingbox'][2],result['boundingbox'][3],result['lat'],result['lon'])
                             bot.sendMessage(usr_id, response,disable_web_page_preview='true')
+                            bot.sendMessage(usr_id, "More info /details{0}".format(result['osm_id']),disable_web_page_preview='true')
                             response = ""
                 elif re.match("/search.*",message) is not None:
                     response = "Please indicate what are you searching"
