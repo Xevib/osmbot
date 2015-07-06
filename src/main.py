27d38fcaa9dea959332430b7e73da865d3f26760
Xavier Barnada <xbarnada@gmail.com>
2015-07-06 20:51:40 +0200

    Refactro & bug fix

--- src/main.py
+++ src/main.py
@@ -6,6 +6,34 @@ import sched, time
 from osmapi import OsmApi
 from bot import OSMbot
 
+def pretty_tags(data):
+    tags = data['tag']
+    response = []
+    t = ""
+    if 'name' in tags:
+        t = "\xF0\x9F\x93\xAE "+str(tags['name'])
+    if 'addr:housenumber' in tags or 'addr:street' in tags or 'addr:city' in tags or 'addr:country' in tags:
+        t += "\n"
+    if 'addr:housenumber' in tags:
+        t += tags['addr:housenumber']+" "
+    if 'addr:street' in tags:
+        t += tags['addr:street']+" "
+    if 'addr:city' in tags:
+        t += tags['addr:city']+" "
+    if 'addr:country' in tags:
+       t += tags['addr:country']+" "
+    if 'phone' in tags:
+        response.append(t)
+        t = ""
+        response.append("\xF0\x9F\x93\x9E "+str(tags['phone']))
+    if 'fax' in tags:
+        t += "\xF0\x9F\x93\xA0 "+str(tags['fax'])
+    if 'email' in tags:
+        t += "\xE2\x9C\x89 "+str(tags['email'])
+    if 'website' in tags:
+        t += "\xF0\x9F\x8C\x8D "+str(tags['website'])
+    response.append(t)
+    return response
 
 def attend(sc):
     if os.path.isfile("last.id"):
@@ -18,91 +46,78 @@ def attend(sc):
     if updates['ok']:
         print "Attending "+str(len(updates["result"]))+" "
         for query in updates['result']:
+            response = []
+            t = ""
             if "text" in query["message"]:
                 message = query["message"]["text"]
                 usr_id = query["message"]["chat"]["id"]
                 if message.startswith("@osmbot"):
                     message = message[8:]
                 if message == "/start":
-                    response = "Hi,how I can help you?"
+                    response = ["Hi,how I can help you?"]
                 elif message.startswith("/details"):
                     id = message[8:].strip()
                     try:
                         osm_data = api.NodeGet(int(id))
                         if osm_data is None:
-                            osm_data = api.WayGet(int(id))
+                            try:
+                                osm_data = api.WayGet(int(id))
+                            except:
+                                osm_data = api.RelationGet(int(id))
                     except:
                         osm_data = None
+                    if osm_data["tag"] == {}:
+                        response = ["Sorry but this element doesn't have tags"]
                     if osm_data is None:
-                            response = 'Sorry but I couldn\'t find any result,check the id'
+                        response.append('Sorry but I couldn\'t find any result,check the id')
                     else:
-                        response = ""
-                        if 'name' in osm_data['tag']:
-                            response = "\xF0\x9F\x93\xAE "+str(osm_data['tag']['name'])
-                        if 'addr:housenumber' in osm_data['tag'] or 'addr:street' in osm_data['tag'] or 'addr:city' in osm_data['tag'] or 'addr:country' in osm_data['tag']:
-                            response += "\n"
-                            if 'addr:housenumber' in osm_data['tag']:
-                                response += osm_data['tag']['addr:housenumber']+" "
-                            if 'addr:street' in osm_data['tag']:
-                                response += osm_data['tag']['addr:street']+" "
-                            if 'addr:city' in osm_data['tag']:
-                                response += osm_data['tag']['addr:city']+" "
-                            if 'addr:country' in osm_data['tag']:
-                                response += osm_data['tag']['addr:country']+" "
-                        if response != "":
-                            bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                        if 'phone' in osm_data['tag']:
-                            response = "\xF0\x9F\x93\x9E "+str(osm_data['tag']['phone'])
-                            bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                        if 'fax' in osm_data['tag']:
-                            response = "\xF0\x9F\x93\xA0 "+str(osm_data['tag']['fax'])
-                            bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                        if 'email' in osm_data['tag']:
-                            response = "\xE2\x9C\x89 "+str(osm_data['tag']['email'])
-                            bot.sendMessage(usr_id, response,disable_web_page_preview='false')
-                        if 'website' in osm_data['tag']:
-                            response = "\xF0\x9F\x8C\x8D "+str(osm_data['tag']['website'])
-                            bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                            response = ""
-                            response += "\xF0\x9F\x93\x8D http://www.openstreetmap.org/?minlat={0}&maxlat={1}&minlon={2}&maxlon={3}&mlat={4}&mlon={5}\n".format(result['boundingbox'][0],result['boundingbox'][1],result['boundingbox'][2],result['boundingbox'][3],result['lat'],result['lon'])+"\n\n\xC2\xA9 OpenStreetMap contributors"
-
+                        response.append(t)
+                        t = ""
+                        messages = pretty_tags(osm_data)
+                        bot.sendMessage(usr_id, messages, disable_web_page_preview='true')
                 elif message.startswith("/about"):
-                    response = "OpenStreetMap bot info:\n\nCREDITS&CODE\n\xF0\x9F\x91\xA5 Author: OSM català (Catalan OpenStreetMap community)\n\xF0\x9F\x94\xA7 Code: https://github.com/Xevib/osmbot\n\xE2\x99\xBB License: GPLv3, http://www.gnu.org/licenses/gpl-3.0.en.html\n\nNEWS\n\xF0\x9F\x90\xA4 Twitter: https://twitter.com/osmbot_telegram\n\nRATING\n\xE2\xAD\x90 Rating&reviews: http://storebot.me/bot/osmbot\n\xF0\x9F\x91\x8D Please rate me at: https://telegram.me/storebot?start=osmbot\n\nThanks for use @OSMbot!!"
+                    response = ["OpenStreetMap bot info:\n\nCREDITS&CODE\n\xF0\x9F\x91\xA5 Author: OSM català (Catalan OpenStreetMap community)\n\xF0\x9F\x94\xA7 Code: https://github.com/Xevib/osmbot\n\xE2\x99\xBB License: GPLv3, http://www.gnu.org/licenses/gpl-3.0.en.html\n\nNEWS\n\xF0\x9F\x90\xA4 Twitter: https://twitter.com/osmbot_telegram\n\nRATING\n\xE2\xAD\x90 Rating&reviews: http://storebot.me/bot/osmbot\n\xF0\x9F\x91\x8D Please rate me at: https://telegram.me/storebot?start=osmbot\n\nThanks for use @OSMbot!!"]
                 elif re.match("/search.*",message) is not None and message[8:] != "":
-                    search = message[8:].replace("\n","").replace("\r","")
-                    response = 'Results for "{0}":\n\n'.format(search)
+                    search = message[8:].replace("\n","").replace("\r", "")
+                    t = 'Results for "{0}":\n\n'.format(search)
                     results = nom.query(search)
                     if len(results) ==0:
-                        response = 'Sorry but I couldn\'t find any result for "{0}" \xF0\x9F\x98\xA2\nBut you can try to improve OpenStreetMap\xF0\x9F\x94\x8D\nhttp://www.openstreetmap.org'.format(search)
-                    if len(results) ==1:
+                        response = ['Sorry but I couldn\'t find any result for "{0}" \xF0\x9F\x98\xA2\nBut you can try to improve OpenStreetMap\xF0\x9F\x94\x8D\nhttp://www.openstreetmap.org'.format(search)]
+                    if len(results) == 1:
                         for result in results:
-                            response += "\xF0\x9F\x93\xAE "+result["display_name"]+"\n"
+                            t += "\xF0\x9F\x93\xAE "+result["display_name"]+"\n"
+                            print "id:"+str(result['osm_id'])+" type:"+str(result['osm_type'])
                             try:
                                 if result['osm_type'] == 'node':
                                     osm_data = api.NodeGet(int(result['osm_id']))
-                                else:
+                                elif result['osm_type'] == 'way':
                                     osm_data = api.WayGet(int(result['osm_id']))
+                                else:
+                                    osm_data = api.RelationWayGet(int(result['osm_id']))
                             except:
                                 osm_data = None
                             if osm_data is not None and 'phone' in osm_data['tag']:
-                                bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                                response = "\xF0\x9F\x93\x9E "+osm_data['tag']['phone']+"\n"
-                                bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                                response = ""
-                            response += "\xF0\x9F\x93\x8D http://www.openstreetmap.org/?minlat={0}&maxlat={1}&minlon={2}&maxlon={3}&mlat={4}&mlon={5}\n".format(result['boundingbox'][0],result['boundingbox'][1],result['boundingbox'][2],result['boundingbox'][3],result['lat'],result['lon'])
-                            bot.sendMessage(usr_id, response,disable_web_page_preview='true')
-                            response = ""
+                                response.append(t)
+                                t = ""
+                                response.append("\xF0\x9F\x93\x9E "+osm_data['tag']['phone']+"\n")
+                        t += "\xF0\x9F\x93\x8D http://www.openstreetmap.org/?minlat={0}&maxlat={1}&minlon={2}&maxlon={3}&mlat={4}&mlon={5}\n".format(result['boundingbox'][0],result['boundingbox'][1],result['boundingbox'][2],result['boundingbox'][3],result['lat'],result['lon'])
                     else:
                         for result in results:
-                            response += "\xE2\x96\xB6 "+result["display_name"]+"\n\n"+"More info /details{0}".format(result['osm_id'])+"\n\n"
-                        response += "\xC2\xA9 OpenStreetMap contributors\n"
+                            print "id:"+str(result['osm_id'])+" type:"+str(result['osm_type'])
+                            t += "\xE2\x96\xB6 "+result["display_name"]+"\n\n"
+                            t += "\xF0\x9F\x93\x8D http://www.openstreetmap.org/?minlat={0}&maxlat={1}&minlon={2}&maxlon={3}&mlat={4}&mlon={5}\n\n".format(result['boundingbox'][0],result['boundingbox'][1],result['boundingbox'][2],result['boundingbox'][3],result['lat'],result['lon'])
+                            t += "More info /details{0}".format(result['osm_id'])+"\n\n"
+
+                        t += "\xC2\xA9 OpenStreetMap contributors\n"
                 elif re.match("/search.*",message) is not None:
-                    response = "Please indicate what are you searching with command /search <search term>"
+                    response = ["Please indicate what are you searching with command /search <search term>"]
                 else:
-                    response = "Use /search <search term> command to indicate what you are searching"
+                    response = ["Use /search <search term> command to indicate what you are searching"]
+                response.append(t)
+                t = ""
                 bot.sendMessage(usr_id, response,disable_web_page_preview='true')
             last_id = query["update_id"]
-            f = open("last.id" , "w")
+            f = open("last.id", "w")
             f.write(str(last_id))
             f.close()
     sc.enter(15, 1, attend, (sc,))
