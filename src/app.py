bc0f2b02e47303730b85dfb2edeb50342b27a172
Xavier Barnada <xbarnada@gmail.com>
2015-08-30 17:35:54 +0200

    Flask blueprints

---
+++ src/app.py
@@ -0,0 +1,20 @@
+import logging
+
+from flask import Flask, request
+from bot import OSMbot
+from configobj import ConfigObj
+
+application = Flask(__name__)
+application.debug = True
+
+config = ConfigObj("bot.conf")
+token = config["token"]
+bot = OSMbot(token)
+
+f = open("certificate.crt", "r")
+cert_data = f.read()
+f.close()
+bot.setWebhook(config['webhook'], cert_data)
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', debug=True)
--- src/osmbot_blueprint.py
+++ src/osmbot_blueprint.py
@@ -5,12 +5,15 @@ import re
 import nominatim
 import sched, time
 from osmapi import OsmApi
-from bot import OSMbot
+
 import urllib
 from configobj import ConfigObj
 from typeemoji import typeemoji
 from maptools import download,genBBOX
 import gettext
+from flask import render_template, current_app, Blueprint
+
+
 
 import user as u
 avaible_languages = ['Catalan', 'English', 'Spanish', 'Swedish']
@@ -19,19 +22,23 @@ application = Flask(__name__)
 application.debug = True
 config = ConfigObj("bot.conf")
 token = config["token"]
-bot = OSMbot(token)
 
-@application.route("/hook/<string:token>",methods=["POST"])
+
+osmbot = Blueprint(
+    'osmbot', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+
+@osmbot.route("/hook/<string:token>", methods=["POST"])
 def attend_webhook(token):
-    current_app.logger.debug("token:%s",token)
+    current_app.logger.debug("token:%s", token)
     if token == config['token']:
         return "OK"
     else:
         return "NOT ALLOWED"
 
 if __name__ == "__main__":
-    f = open("certificate.crt","r")
-    cert_data = f.read()
-    f.close()
-    bot.setWebhook(config['webhook'],'')
+
     application.run(host='0.0.0.0')
