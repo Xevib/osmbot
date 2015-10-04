8a13ce62d1b850cf23b5063b3c91e919a80272ea
Xavier Barnada <xbarnada@gmail.com>
2015-10-04 20:34:52 +0200

    Code formating

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ application = Flask(__name__)
 application.debug = True
 Osmbot(application, '')
 
-config = ConfigObj("bot.conf")
-token = config["token"]
+config = ConfigObj('bot.conf')
+token = config['token']
 bot = OSMbot(token)
 
-f = open("nginx.crt", "r")
+f = open('nginx.crt', 'r')
 cert_data = f.read()
 f.close()
 webhook = os.path.join(config['webhook'], config['token'])
-application.logger.debug("webhook:%s", config['webhook'])
+application.logger.debug('webhook:%s', config['webhook'])
 response = bot.setWebhook(webhook, cert_data)
-application.logger.debug("response:%s", response)
+application.logger.debug('response:%s', response)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
