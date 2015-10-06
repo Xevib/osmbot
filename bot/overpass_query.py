7129f69886339dd0848bd932388632265e6ed168
Xavier Barnada <xbarnada@gmail.com>
2015-10-06 08:40:49 +0200

    Fix dict

--- bot/overpass_query.py
+++ bot/overpass_query.py
@@ -1,6 +1,6 @@
 type_query = {
-    {'hospital':
+    'hospital':
          {'query':'node["man_made"="surveillance"]({0});way["man_made"="surveillance"]({0});relation["man_made"="surveillance"]({0});',
           'distance':10000}
-     }
+     
 }
