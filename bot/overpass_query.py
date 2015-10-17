e5d84c920030186565527db807d4b9bf9294eca0
Xavier Barnada <xbarnada@gmail.com>
2015-10-17 22:17:02 +0200

    Add desfibrilator

--- bot/overpass_query.py
+++ bot/overpass_query.py
@@ -1,6 +1,8 @@
 type_query = {
     'hospital':
-         {'query': 'node["amenity"="hospital"]({0});way["amenity"="hospital"]({0});relation["amenity"="hospital"]({0});',
-          'distance': 10000}
+         {'query': 'node["amenity"="hospital"]({0});way["amenity"="hospital"]({0});relation["amenity"="hospital"]({0});', 'distance': 10000},
+     'desfibrilador':
+         {'query': 'node["emergency"="defibrillator"]({0});way["emergency"="defibrillator"]({0});relation["emergency"="defibrillator"]({0});', 'distance': 10000}
+
 
 }
