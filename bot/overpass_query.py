dcb965dfe8973495bb95e5ac76d0db01f64ff657
Xavier Barnada <xbarnada@gmail.com>
2015-10-17 22:34:06 +0200

    update and translate queries

--- bot/overpass_query.py
+++ bot/overpass_query.py
@@ -1,8 +1,9 @@
 type_query = {
     'hospital':
-         {'query': 'node["amenity"="hospital"]({0});way["amenity"="hospital"]({0});relation["amenity"="hospital"]({0});', 'distance': 10000},
-     'desfibrilador':
-         {'query': 'node["emergency"="defibrillator"]({0});way["emergency"="defibrillator"]({0});relation["emergency"="defibrillator"]({0});', 'distance': 10000}
-
+        {'query': 'node["amenity"="hospital"]({0});way["amenity"="hospital"]({0});relation["amenity"="hospital"]({0});','distance': 10000},
+    'bar':
+        {'query': 'node["amenity"="bar"]({0});way["amenity"="bar"]({0});relation["amenity"="bar"]({0});', 'distance': 10000},
+    'defibrillator':
+        {'query': 'node["emergency"="defibrillator"]({0});way["emergency"="defibrillator"]({0});relation["emergency"="defibrillator"]({0});', 'distance': 10000}
 
 }
