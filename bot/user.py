a4341be86fd74f82170f67db6700761e7c2aa62a
Xavier Barnada <xbarnada@gmail.com>
2015-11-17 18:56:52 +0100

    First attempt to switch to Pg

--- bot/user.py
+++ bot/user.py
@@ -1,4 +1,5 @@
-import sqlite3
+import psycopg2
+from psycopg2.extras import DictCursor
 from hashlib import sha1
 
 
@@ -8,16 +9,18 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+
 class User(object):
 
-    def __init__(self, database):
-        self.conn = sqlite3.connect(database)
-        self.conn.row_factory = dict_factory
-        #CREATE TABLE user (id int, mode varchar(30),zoom int,format varchar(30),language varchar(10))
-    def get_user(self, id):
-        shaid = sha1(str(id)).hexdigest()
-        cur = self.conn.cursor()
-        cur.execute("SELECT * FROM user WHERE shaid = ? LIMIT 1", (shaid,))
+    def __init__(self, host, database, user, password):
+        self.conn = psycopg2.connect(database=database, user=user, password=password, host=host)
+
+        #CREATE TABLE users (id int, mode varchar(30),zoom int,format varchar(30),language varchar(10))
+
+    def get_user(self, identifier):
+        shaid = sha1(str(identifier)).hexdigest()
+        cur = self.conn.cursor(cursor_factory=DictCursor)
+        cur.execute('SELECT * FROM user WHERE shaid = %s LIMIT 1', (shaid,))
         data = cur.fetchone()
         cur.close()
         if data is None:
@@ -33,21 +36,21 @@ class User(object):
                 data['lang_set'] = False
             return data
 
-    def set_field(self, id, field, value):
-        shaid = sha1(str(id)).hexdigest()
-        cur = self.conn.cursor()
-        cur.execute("SELECT count(shaid) as count FROM user WHERE shaid = ?", (shaid,))
+    def set_field(self, identifier, field, value):
+        shaid = sha1(str(identifier)).hexdigest()
+        cur = self.conn.cursor(cursor_factory=DictCursor)
+        cur.execute("SELECT count(shaid) as count FROM user WHERE shaid = %s", (shaid,))
         num = cur.fetchone()
         if num['count'] == 0:
-            cur.execute("INSERT INTO user (shaid,{0}) VALUES (?,?)".format(field), (shaid, value))
+            cur.execute("INSERT INTO user (shaid,{0}) VALUES (%s,%s)".format(field), (shaid, value))
         else:
-            cur.execute("UPDATE user SET {0} = ? WHERE shaid = ? ".format(field), (value, shaid))
+            cur.execute("UPDATE user SET {0} = %s WHERE shaid = %s ".format(field), (value, shaid))
         self.conn.commit()
         cur.close()
         return cur.rowcount != 0
 
     def get_defaultconfig(self):
-        return {'lang': 'en', 'mode': 'normal','lang_set':False}
+        return {'lang': 'en', 'mode': 'normal', 'lang_set': False}
 
     def close(self):
         self.conn.close()
