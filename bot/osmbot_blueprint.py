e97ee4bb61206e1d867a7fe08a95959fdb4b485d
Xavier Barnada <xbarnada@gmail.com>
2015-12-02 22:20:22 +0100

    Fix message on download errors

--- bot/osmbot_blueprint.py
+++ bot/osmbot_blueprint.py
@@ -399,9 +399,12 @@ def MapCommand(message, chat_id, user_id, user, zoom=None, imgformat='png', lat=
             if res:
                 bbox = res[0]['boundingbox']
                 auto_scale = getScale([bbox[0],bbox[2],bbox[1],bbox[3]])
-
-                data = download([bbox[2],bbox[0],bbox[3],bbox[1]], _, scale=auto_scale )
-                bot.sendPhoto(chat_id, data, 'map.png', 'Map')
+                try:
+                    data = download([bbox[2],bbox[0],bbox[3],bbox[1]], _, scale=auto_scale )
+                except ValueError as v:
+                    response.append(v.message)
+                else:
+                    bot.sendPhoto(chat_id, data, 'map.png', 'Map')
             else:
                 response.append(_("Sorry, I can't understand you") + ' \xF0\x9F\x98\xB5\n' +
                                 _('Perhaps I could help you with the command /help') + ' \xF0\x9F\x91\x8D')
