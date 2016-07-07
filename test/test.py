469c01baa24f4fbc412a02989a932ba3f602136b
Xevi <xbarnada@gmail.com>
2016-07-07 08:40:46 +0200

    Fix test

--- test/test.py
+++ test/test.py
@@ -4,10 +4,11 @@ import os
 
 class BotTest(unittest.TestCase):
     def test_languages(self):
-        from bot.osmbot_blueprint import avaible_languages
+        from bot import osmbot_blueprint
+        print os.getcwd()
         lang_dirs = os.listdir('../bot/locales')
         for lang_dir in lang_dirs:
-            self.assertTrue(lang_dir in avaible_languages.values())
+            self.assertTrue(lang_dir in osmbot_blueprint.avaible_languages.values())
 
 if __name__ == '__main__':
     unittest.main()
