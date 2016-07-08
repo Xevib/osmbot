198d49771e6da9adca98b2c032d4b0a8796b1f4f
Xevi <xbarnada@gmail.com>
2016-07-08 20:15:13 +0200

    Improve configuration

--- bot/osmbot_blueprint.py
+++ bot/osmbot_blueprint.py
@@ -31,10 +31,11 @@ application = Flask(__name__)
 application.debug = True
 config = ConfigObj('bot.conf')
 
+if config:
+    token = config.get('token', '')
+    user = u.User(config.get('host', ''), config.get('database', ''), config.get('user', ''), config.get('password', ''))
+    bot = OSMbot(token)
 
-token = config.get('token', '')
-user = u.User(config.get('host',''), config.get('database', ''), config.get('user',''), config.get('password',''))
-bot = OSMbot(token)
 api = OsmApi()
 nom = pynominatim.Nominatim()
 
