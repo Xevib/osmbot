8d963dbb74b1cbec62560b9dbe02455f661fa152
Xevi <xbarnada@gmail.com>
2016-07-08 07:37:28 +0200

    Fix language tests

--- test/test.py
+++ test/test.py
@@ -6,8 +6,10 @@ class BotTest(unittest.TestCase):
     def test_languages(self):
         print os.getcwd()
         from bot import avaible_languages
-        lang_dirs = os.listdir('../bot/locales')
+        lang_dirs = os.listdir('bot/locales')
         for lang_dir in lang_dirs:
+            if lang_dir not in avaible_languages.values():
+                print '{} not found in directory in avaible languages but found in bo/locales'.format(lang_dir)
             self.assertTrue(lang_dir in avaible_languages.values())
 
 if __name__ == '__main__':
