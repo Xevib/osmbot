2307f58d104d94828f94b378d6b995f8f2d43a57
Xevi <xbarnada@gmail.com>
2016-07-17 22:18:15 +0200

    Fix function

--- bot/osmbot_blueprint.py
+++ bot/osmbot_blueprint.py
@@ -190,7 +190,7 @@ def SearchCommand(message, user_config, chat_id):
         text = get_template('not_found_message.md').render(search=search)
         m = Message(chat_id, text, parse_mode='Markdown')
         bot.sendMessage(m,)
-        return 
+        return
     else:
         t = _('Results for') + ' "{0}":\n\n'.format(search)
         for result in results[:10]:
