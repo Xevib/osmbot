0af83704d031b8d64b36c850239995dd5a3df42a
Xevi <xbarnada@gmail.com>
2016-07-18 07:51:09 +0200

    Fix tests

--- test/test.py
+++ test/test.py
@@ -10,12 +10,13 @@ sys.setdefaultencoding('utf-8')
 
 class BotTest(unittest.TestCase):
     def test_languages(self):
-        from bot.osmbot_blueprint import avaible_languages
+        from bot.osmbot import OsmBot
+        b = OsmBot({})
         lang_dirs = os.listdir('bot/locales')
         for lang_dir in lang_dirs:
-            if os.path.isdir(os.path.join('bot/locales', lang_dir)) and lang_dir not in avaible_languages.values():
+            if os.path.isdir(os.path.join('bot/locales', lang_dir)) and lang_dir not in b.get_languages().values():
                 print '{} not found in directory in avaible languages but found in bo/locales'.format(lang_dir)
-                self.assertTrue(lang_dir in avaible_languages.values())
+                self.assertTrue(lang_dir in b.get_languages().values())
 
     def test_templates(self):
         from jinja2 import Environment, exceptions
