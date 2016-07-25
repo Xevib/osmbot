b1eb8cb07ba33718443f618c57d9d3ba9c33f5b7
edgarriba <edgar.riba@gmail.com>
2016-07-25 21:36:43 +0200

    test fake config file

--- test/test.py
+++ test/test.py
@@ -18,15 +18,38 @@ class BotTest(unittest.TestCase):
     # instantiate OsmBotMock class
     b = OsmBotMock({}, auto_init=False)
 
-    def test_init_config(self):
+    def test_config_file(self):
+        from configobj import ConfigObj
+        config = ConfigObj('test/bot.conf')
+        
+        self.assertEqual(config['database'  ], 'bot')
+        self.assertEqual(config['user'      ], 'osmbot')
+        self.assertEqual(config['host'      ], 'localhost')
+        self.assertEqual(config['password'  ], '1234')
+        self.assertEqual(config['hooks'     ], 'true')
+        self.assertEqual(config['webhook'   ], 'https://example.com:443/hook/')
+        self.assertEqual(config['sentry_dsn'], 'sync+http://aadfsaa5@example.com:9000/1')
+        self.assertEqual(config['token'     ], '32111224414:AAF0BqwSgFKTzkgTkJLcLBKVb2ebrSXbWX4')
+        self.assertEqual(config['update_interval'], '10')
+
+    def test_init_config_error(self):
         from configobj import ConfigObj
         config = ConfigObj()
  
         # no config and empty config
         self.assertRaises(OSMError, self.b.init_config, 0)
         self.assertRaises(OSMError, self.b.init_config, True)
-        self.assertRaises(OSMError, self.b.init_config, 'random.cfg')
+        self.assertRaises(OSMError, self.b.init_config, 'random_string')
         self.assertRaises(OSMError, self.b.init_config, config)
+
+    def test_init_config(self):
+        from configobj import ConfigObj
+        config = ConfigObj('test/bot.conf')
+
+        # TODO(edgar/xevi): let's see if we can test the
+        # real api with a fake config
+
+        # self.b.init_config(config)
 
     def test_languages(self):
         lang_dirs = os.listdir('bot/locales')
