6dcd118b461f3a87ef94ee1ef045296518491e63
Xevi <xbarnada@gmail.com>
2016-07-26 20:42:51 +0200

    Add message to test

---
+++ test/messages.py
@@ -0,0 +1,31 @@
+messages = [
+    {u'message':
+         {u'from':
+              {
+                  u'username': u'luke',
+                  u'first_name': u'Luke',
+                  u'last_name': u'Skywalker',
+                  u'id': 31416
+              },
+             u'text': u'/search rancho skywalker',
+             u'entities':
+                 [
+                     {
+                         u'length': 7,
+                         u'type': u'bot_command',
+                         u'offset': 0
+                     }
+                 ],
+             u'chat': {
+                 u'username': u'luke',
+                 u'first_name': u'Luke',
+                 u'last_name': u'Skywalker',
+                 u'type': u'private',
+                 u'id': 31416
+             },
+             u'date': 1469558172,
+             u'message_id': 1234
+         },
+        u'update_id': 12345
+    }
+]
