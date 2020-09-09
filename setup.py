ebd9b7d92b3750db6996ad9c2e2118f7c9e420da
Xavier Barnada <xbarnada@gmail.com>
2020-09-09 15:51:42 +0200

    Release v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='osmbot',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='http://git.gisce.net/gis/webgis',
     license='MIT',
