5f45fed825d2a0f1f05ac8428589140e185bb7a5
Xavier Barnada <xbarnada@gmail.com>
2021-08-19 19:08:36 +0200

    Fix empty message

--- bot/osmbot_blueprint.py
+++ bot/osmbot_blueprint.py
@@ -120,9 +120,12 @@ def attend_webhook(token):
                     message = message.replace('@osmbot', '')
                     message = message.replace('@OSMbot', '')
             if not 'callback_query' in query:
-                message = osmbot.clean_message(message)
-                osmbot.load_language(user_config['lang'])
-                osmbot.answer_message(message, query, chat_id, user_id, user_config, user, message_type)
+                if message:
+                    message = osmbot.clean_message(message)
+                    osmbot.load_language(user_config['lang'])
+                    osmbot.answer_message(message, query, chat_id, user_id, user_config, user, message_type)
+                else:
+                    return 'OK'
             else:
                 osmbot.answer_callback(query)
             return 'OK'
